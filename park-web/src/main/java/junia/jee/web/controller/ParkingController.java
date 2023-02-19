c04db2ddd7ac551fe6620b540e6da875997a4ed7
profotoce <romain.vanhee@gmail.com>
2023-02-19 23:42:11 +0100

    final push

--- park-web/src/main/java/junia/jee/web/controller/ParkingController.java
+++ park-web/src/main/java/junia/jee/web/controller/ParkingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -20,12 +21,14 @@ public class ParkingController {
     LocalisationService localisationService;
 
     @RequestMapping(value = "/parking/{id}",method = RequestMethod.GET)
-    public String getParking(@PathVariable("id") int id, ModelMap modelMap) {
+    public String getParking(@PathVariable("id") int id, ModelMap modelMap) throws IOException {
         Optional<Localisation> localisation = localisationService.getLocalisationById(id);
         Parking parking = localisation.get().getParking();
+        String currentPath = new java.io.File(".").getCanonicalPath();
         modelMap.addAttribute("parking", parking);
         modelMap.addAttribute("localisation", localisation);
         modelMap.addAttribute("adresse", localisation.get().getAdresse());
+        modelMap.addAttribute("currentDir", currentPath);
         String photoPath = localisation.get().getPhoto();
         photoPath = photoPath.substring(5);
         modelMap.addAttribute("photoPath", photoPath);
